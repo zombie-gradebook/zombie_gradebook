6c91da9806d0bfc2d315375f05ff3966d7892098
Anthony Ellis <ajellis1989@gmail.com>
2015-10-09 14:13:53 -0400

    Fixes students and teacher associations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
   has_secure_password
+  belongs_to :teacher
 
   validates :name, presence: true
   validates :email, presence: true
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,7 @@
 class Teacher < ActiveRecord::Base
 
 has_secure_password
+has_many :students
 
 validates :name, presence: true
 validates :email, presence: true
