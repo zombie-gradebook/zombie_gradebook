cbd5ee9de77a48930daf06390184160907348078
Anthony Ellis <ajellis1989@gmail.com>
2015-10-09 12:44:53 -0400

    Adds completed Teachers scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-    Teacher.new(name: "Patrick Swayze", email: "roadhouse@email.com", password: "pword")
-    Teacher.new(name: "Jace Beleren", email: "mind_sculptor@mtg.com", password: "pword")
+    Teacher.create(name: "Patrick Swayze", email: "roadhouse@email.com", password: "pword")
+    Teacher.create(name: "Jace Beleren", email: "mind_sculptor@mtg.com", password: "pword")
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
