9f041d0883be23fc6e66a6b5dc5c1df25f7a145c
Anthony Ellis <ajellis1989@gmail.com>
2015-10-09 14:38:28 -0400

    Adds appropriate associations

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,6 @@
 class Parent < ActiveRecord::Base
   has_secure_password
+  belongs_to :student
   validates :name, presence: true
   validates :email, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ActiveRecord::Base
   has_secure_password
   belongs_to :teacher
+  has_many :parents
+  has_many :grades
 
   validates :name, presence: true
   validates :email, presence: true
