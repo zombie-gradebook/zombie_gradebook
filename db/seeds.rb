b469ca5bc6a5dd9a381bf7ac4b6a8518ed741d40
Doug Bryant <douglas.e.bryant@gmail.com>
2015-10-09 12:08:25 -0400

    Create parents scaffold and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+reed = Parent.create!(name: "Mr. Reed", email: "reed@parent.com", password: "pword")
+mann = Parent.create!(name: "Mrs. Mann", email: "mann@parent.com", password: "pword")
